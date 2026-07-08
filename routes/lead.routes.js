9e91fa34a8008129617b01e2508768af3213bbcb
Beaskyy <afeez.akintobi@yahoo.com>
2026-07-08 12:24:53 +0100

    first commit

---
+++ routes/lead.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+  createLead,
+  deleteLead,
+  getLead,
+  getLeads,
+  reorderLeads,
+  updateLead,
+} from "../controllers/lead.controller.js";
+import { protect } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router.use(protect);
+
+router.patch("/reorder", reorderLeads);
+router.route("/").get(getLeads).post(createLead);
+router.route("/:id").get(getLead).put(updateLead).delete(deleteLead);
+
+export default router;
