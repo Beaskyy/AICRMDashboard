e5e3c2a791bb98214ebd4130a1c987db793d4444
Beaskyy <afeez.akintobi@yahoo.com>
2026-07-10 21:05:11 +0100

    feat: add getOverview controller to aggregate dashboard analytics data

---
+++ controllers/analytics.controller.js
@@ -0,0 +1,18 @@
+import { Contact } from "../models/Contact.js";
+import { Lead } from "../models/Lead.js";
+import { Task } from "../models/Task.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const getOverview = asyncHandler(async (req, res) => {
+  const owner = req.user.id
+
+  const [leads, contactCount, openTasks] = await Promise.all([
+    Lead.find({ owner })
+    Contact.countDocuments({ owner })
+    Task.countDocuments({ owner, status: { $ne: "Completed"}})
+  ])
+
+  const stages = ["New", "Qualified", "Proposal", "Won", "Lost"]
+  const byStage = Object.fromEntries(stages.map((s) => [s, {count: 0, value: 0}]))
+  
+})
